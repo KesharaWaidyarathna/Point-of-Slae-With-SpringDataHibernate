811681fd6f51772516aadd491285e9fa289f6aa8
keshara1997 <waidyrathna@gmail.com>
2019-11-14 12:58:30 +0530

    Rewamp the first stage of DAO
    	modified:   src/lk/ijse/dep/pos/dao/CrudDAO.java
    	modified:   src/lk/ijse/dep/pos/dao/custom/impl/CustomerDAOImpl.java
    	modified:   src/lk/ijse/dep/pos/dao/custom/impl/ItemDAOImpl.java
    	modified:   src/lk/ijse/dep/pos/dao/custom/impl/OrderDAOImpl.java
    	modified:   src/lk/ijse/dep/pos/dao/custom/impl/OrderDetailDAOImpl.java
    	modified:   src/lk/ijse/dep/pos/dao/custom/impl/QueryDAOImpl.java

--- src/lk/ijse/dep/pos/dao/custom/impl/QueryDAOImpl.java
+++ src/lk/ijse/dep/pos/dao/custom/impl/QueryDAOImpl.java
@@ -12,7 +12,8 @@ public class QueryDAOImpl implements QueryDAO {
 
     @Override
     public CustomEntity getOrderInfo(int orderId) throws Exception {
-        ResultSet rst = CrudUtil.execute("SELECT C.customerId, C.name, O.date  FROM Customer C INNER JOIN `Order` O ON C.customerId=O.customerId WHERE O.id=?", orderId);
+        return null;
+       /* ResultSet rst = CrudUtil.execute("SELECT C.customerId, C.name, O.date  FROM Customer C INNER JOIN `Order` O ON C.customerId=O.customerId WHERE O.id=?", orderId);
         if (rst.next()){
             return new CustomEntity(orderId,
                     rst.getString(1),
@@ -20,12 +21,13 @@ public class QueryDAOImpl implements QueryDAO {
                     rst.getDate(3));
         }else{
             return null;
-        }
+        }*/
     }
 
     @Override
     public CustomEntity getOrderInfo2(int orderId) throws Exception {
-        ResultSet rst = CrudUtil.execute("SELECT O.id, C.customerId, C.name, O.date, SUM(OD.qty * OD.unitPrice) AS Total  FROM Customer C INNER JOIN `Order` O ON C.customerId=O.customerId\" +\n" +
+        return null;
+       /* ResultSet rst = CrudUtil.execute("SELECT O.id, C.customerId, C.name, O.date, SUM(OD.qty * OD.unitPrice) AS Total  FROM Customer C INNER JOIN `Order` O ON C.customerId=O.customerId\" +\n" +
                 "                \" INNER JOIN OrderDetail OD on O.id = OD.orderId WHERE O.id=? GROUP BY orderId", orderId);
         if (rst.next()){
             return new CustomEntity(rst.getInt(1),
@@ -35,12 +37,14 @@ public class QueryDAOImpl implements QueryDAO {
                     rst.getDouble(5));
         }else{
             return null;
-        }
+        }*/
     }
 
     @Override
     public List<CustomEntity> getOrdersInfo(String query) throws Exception {
-        ResultSet rst = CrudUtil.execute("SELECT O.id, C.customerId, C.name, O.date, SUM(OD.qty * OD.unitPrice) AS Total  FROM Customer C INNER JOIN `Order` O ON C.customerId=O.customerId " +
+        return null;
+    }
+     /*   ResultSet rst = CrudUtil.execute("SELECT O.id, C.customerId, C.name, O.date, SUM(OD.qty * OD.unitPrice) AS Total  FROM Customer C INNER JOIN `Order` O ON C.customerId=O.customerId " +
                 "INNER JOIN OrderDetail OD on O.id = OD.orderId WHERE O.id LIKE ? OR C.customerId LIKE ? OR C.name LIKE ? OR O.date LIKE ? GROUP BY O.id", query,query,query,query);
         List<CustomEntity> al = new ArrayList<>();
         while (rst.next()){
@@ -51,5 +55,5 @@ public class QueryDAOImpl implements QueryDAO {
                     rst.getDouble(5)));
         }
         return al;
-    }
+    }*/
 }
