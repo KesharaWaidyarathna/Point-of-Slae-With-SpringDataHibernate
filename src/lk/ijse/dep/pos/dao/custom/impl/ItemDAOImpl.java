9aa2b4eafaf6a8c170ecf1bdd8f31c05194e2f2b
keshara1997 <waidyrathna@gmail.com>
2019-11-26 10:38:41 +0530

    Done ORM intergration and revamp the DAO
    	new file:   src/lk/ijse/dep/pos/HibernateConfig.java

--- src/lk/ijse/dep/pos/dao/custom/impl/ItemDAOImpl.java
+++ src/lk/ijse/dep/pos/dao/custom/impl/ItemDAOImpl.java
@@ -5,11 +5,12 @@ import lk.ijse.dep.pos.dao.custom.ItemDAO;
 import lk.ijse.dep.pos.entity.Item;
 import org.hibernate.Session;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
-@Component
+@Repository
 public class ItemDAOImpl extends CrudDAOImpl<Item,String> implements ItemDAO {
 
     public String getLastItemCode() throws Exception {
